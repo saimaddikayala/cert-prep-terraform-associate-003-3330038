1dd16081704d8de2a86cbe3ade958507c792bee6
Sai Maddikayala <sai.maddikayala@takeda.com>
2025-02-16 15:38:44 -0500

    modified main.tf

--- providers.tf
+++ providers.tf
@@ -2,20 +2,19 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 5.31.0"
+      version = "5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "terraform"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west"
   region = "us-west-1"
-  profile = "Helen"
+  profile = "terrafrom"
 }
-
